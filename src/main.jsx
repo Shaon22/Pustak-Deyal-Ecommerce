5177ecaf748b4d84487522aad35ab950f8261dc4
shaon22 <shaonroy11@gmail.com>
2025-07-03 02:06:55 +0600

    set up raect router dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './Routes/Routes.jsx'
+import AuthProvider from './AuthProvider/AuthProvider.jsx'
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const queryClient = new QueryClient()
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router}>
+        </RouterProvider>
+      </QueryClientProvider>
+    </AuthProvider>
+  </React.StrictMode>
 )
